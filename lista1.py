c69323971f38657620f1bcd5c6deb20b50124444
João <feitosa.joao@escolar.ifrn.edu.br>
2025-04-19 20:45:33 +0000

    Listas py

--- lista1.py
+++ lista1.py
@@ -3,4 +3,4 @@ print('João Gabriel Figueiredo Feitosa, 20241011110033')
 # 2
 
 nome = input("Digite seu primeiro nome: ")
-print("Bem-vindo(a) ao Codespaces,",nome)
+print("Bem-vindo(a) ao Codespaces,", nome)
